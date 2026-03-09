bb8a0081992fcb5f3cc384fbd7fc7251199ead80
Azuryy11 <sebastien.lemeu@gmail.com>
2026-03-09 10:05:54 +0100

    Update frontend UI and dependencies

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,6 +4,9 @@ import LogoPWhite from '../img/Logo-P-White.svg';
 import videoVTimesMp4 from '../img/Vidéo_pres_VTimes.mp4';
 import Section from '../components/Section';
 import './About.css';
+import Pole_Production from '../img/Pole_Production2.jpeg';
+import Pole_Coordination from '../img/Pole_Coordination.jpeg';
+import Pole_Communication from '../img/Pole_Communication.jpeg';
 
 function About() {
     return (
@@ -41,6 +44,36 @@ function About() {
 
         <Section
             size='M'
+            title="Présentation"> 
+            <div className="about-text">
+                <p>Entreprise étudiante indépendante, V-Time’s se développe grâce à l’engagement et à la créativité de ses membres, mais également grâce au soutien du public. Chaque création réalisée participe à faire vivre notre projet et à promouvoir une démarche à la fois écologique et artistique. En donnant une seconde vie à des vinyles destinés à être jetés, nous nous inscrivons dans une logique de consommation responsable, tout en proposant des objets uniques et personnalisés. Soutenir V-Time’s, c’est encourager une initiative jeune, engagée et innovante, où la créativité se met au service de l’environnement.</p>
+            </div>
+        </Section>
+
+        <Section
+                size='M'
+                context="services"
+                title="Nos services"
+            >
+                <Card
+                    image={Pole_Production}
+                    title="Pôle Production"
+                    subtitle="Notre équipe de production se charge de récupérer des vinyles abimés ou inutilisés pour les transformer en beaux objets décoratifs. Peintures, paillettes, colles, aiguilles… dites-leur ce que vous souhaitez et ils vous le créent"
+                />
+                <Card
+                    image={Pole_Communication}
+                    title="Pôle Marketing-Communication"
+                    subtitle="Etudes de marché, enquêtes clients, création des visuels, animation des réseaux sociaux, site internet … les missions sont nombreuses et variées, mais ne font pas peur à notre équipe de choc"
+                />
+                <Card
+                    image={Pole_Coordination}
+                    title="Pôle Gestion-Finance"
+                    subtitle="Nos étudiants en BTS Comptabilité-Gestion mettent leurs talents de gestionnaires et de comptables au service de V-Time’s ! Comparaison des coûts, achats, fixation des prix de vente, fixation du seuil de rentabilité, n’ont plus de secret pour eux"
+                />
+            </Section>
+
+        <Section
+            size='L'
             title="Vidéo de Présentation"> 
             <div className="about-video-row">
                         <a
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,13 @@ import Section from '../components/Section';
 import './Home.css'
 import bgVtimes from '../img/bg-vtimes.png';
 import bgVtimesVideo from '../img/bg-vtimes.mp4';
-import photoGroupe from '../img/photo-groupe.jpeg';
+import photoGroupe from '../img/photo-groupe2.jpeg';
 import Card from '../components/cards';
 import IconCard from '../components/IconCard';
 import recycle from '../img/recycle.svg';
-import Pole_Production from '../img/Pole_Production2.jpeg';
-import Pole_Coordination from '../img/Pole_Coordination.jpeg';
-import Pole_Communication from '../img/Pole_Communication.jpeg';
+import Pole_Production from '../img/Pole_Production3.jpeg';
+import Pole_Coordination from '../img/Pole_Communication3.jpeg';
+import Pole_Communication from '../img/Pole_Communication2.jpeg';
 
 function Home() {
 
@@ -28,7 +28,7 @@ function Home() {
                 buttonLink="/products"
             />
             <Section
-                size='L'
+                size='M'
                 context="services"
                 title="Nos services"
             >
@@ -49,35 +49,37 @@ function Home() {
                 />
             </Section>
             <Section
+                size='L'> 
+                <div className="backstage-text">
+                    <p>C’est au sein du Campus Beaupeyrat que le projet V-Time’s a vu le jour, porté par la collaboration des étudiants de première année en BTS MCO, SIO et Comptabilité. Réunis autour d’un objectif commun, ils mettent en œuvre leurs compétences complémentaires pour concevoir, développer et valoriser une véritable mini-entreprise, de l’idée initiale jusqu’à sa concrétisation.</p>
+                    <p>Encadré par une échéance fixée, ce projet s’inscrit dans une démarche professionnalisante, aboutissant à une présentation devant un jury lors d’un concours. Chaque étape — de la création du produit à la communication, en passant par la gestion et le développement du site internet — permet d’appliquer concrètement les enseignements théoriques. V-Time’s devient ainsi bien plus qu’un projet scolaire : une expérience immersive qui prépare les étudiants aux réalités du monde professionnel.</p>
+                </div>
+            </Section>
+            <Section
                 size="M"
-                context="products"
-                subtitle="Nos services"
+                context="services"
                 title="Un savoir-faire unique"
                 description="Chez V-Time’s, notre savoir-faire repose sur la revalorisation de vinyles abîmés en objets décoratifs uniques et personnalisés.
 Nous transformons des disques inutilisables en créations originales telles que des décorations murales et des cadeaux sur mesure, en privilégiant une approche artisanale et responsable.
 V-Time’s associe ainsi écologie et créativité pour donner une seconde vie à la musique du passé."
                 image={bgVtimes}
             />
-            <Section
-                size="S"
-                title="Recyclons ensemble"
-            />
-            <Section
-                size="M-alt"
-                context="engagement"
-            >
-                <IconCard
-                    icon={recycle}
-                    title="Recyclage"
-                    subtitle="Découvrez notre collection exclusive d'horloges"
-                />
-                <IconCard
-                    icon={recycle}
-                    title="Horloges"
-                    subtitle="Découvrez notre collection exclusive d'horloges"
-                />
+            {/* <Section
+                    size="S"
+                    title="Recyclons ensemble" /><Section
+                        size="M-alt"
+                        context="engagement"
+                    >
+                        <IconCard
+                            icon={recycle}
+                            title="Recyclage"
+                            subtitle="Découvrez notre collection exclusive d'horloges" />
+                        <IconCard
+                            icon={recycle}
+                            title="Horloges"
+                            subtitle="Découvrez notre collection exclusive d'horloges" />
 
-            </Section>
+                    </Section> */}
         </div>
     )
 }
